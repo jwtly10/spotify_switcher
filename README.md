19df2a0b17a494008f35d0fdf21f9a0a13d93570
Josh Watley <watleyjosh@gmail.com>
2023-07-20 19:53:31 +0100

    Storing results to CSV

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # spotify_switcher
-Application to make switching music libraries easier
+Application to make switching music services easier
+
+Outputs a CSV list that can be used as input for a webscraper to collate a new playlist in another streaming platform. 
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/joho/godotenv"
 
@@ -26,18 +27,25 @@ func main() {
 
 	savedTracks := getPlaylistTracks(spotifyToken, os.Getenv("PLAYLIST_ID"))
 
-	fmt.Println(savedTracks)
+	writeResultsToCSV(savedTracks)
+	fmt.Println("Done!")
+
 }
 
-func getPlaylistTracks(apiToken string, playlistID string) string {
+func getPlaylistTracks(apiToken string, playlistID string) []string {
+	fmt.Println("Getting playlist tracks...")
 	endpoint := "https://api.spotify.com/v1/playlists/" + playlistID + "/tracks"
 
 	results := []string{}
 	results = recursivelyGetResults(results, endpoint, apiToken)
 
-	fmt.Println(len(results))
+	// fmt.Println(len(results))
 
-	return ""
+	// for x := 0; x < 10; x++ {
+	// 	fmt.Println(results[x])
+	// }
+
+	return results
 }
 
 func recursivelyGetResults(results []string, url_call string, apiToken string) []string {
@@ -65,8 +73,19 @@ func recursivelyGetResults(results []string, url_call string, apiToken string) [
 
 	sizeOfResults, err := jsonParsed.Search("items", "track").ArrayCount()
 
+	artists := []string{}
 	for x := 0; x < sizeOfResults; x++ {
-		results = append(results, jsonParsed.Search("items", "track").Index(x).Path("name").Data().(string))
+		name := strings.ReplaceAll(jsonParsed.Search("items", "track").Index(x).Path("name").Data().(string), ",", " ")
+		numberOfArtists, err := jsonParsed.Search("items", "track").Index(x).Path("artists").ArrayCount()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for y := 0; y < numberOfArtists; y++ {
+			artists = append(artists, jsonParsed.Search("items", "track").Index(x).Path("artists").Index(y).Path("name").Data().(string))
+		}
+		results = append(results, name+", "+strings.Join(artists, ", "))
+		artists = nil
 	}
 
 	if jsonParsed.Search("next").Data() != nil {
@@ -74,4 +93,19 @@ func recursivelyGetResults(results []string, url_call string, apiToken string) [
 	}
 
 	return results
+}
+
+func writeResultsToCSV(result []string) {
+	csvFile, err := os.Create("results.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer csvFile.Close()
+
+	for _, row := range result {
+		_, err := csvFile.WriteString(row + "\n")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
